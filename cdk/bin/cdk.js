3f386e96f1de8bfc851f197eafff1a905236395d
Joel Bernerman <joel.bernerman@helsingborg.se>
2021-02-17 15:46:21 +0100

    Use subdirectory

--- cdk/bin/cdk.js
+++ cdk/bin/cdk.js
@@ -43,8 +43,9 @@ class PipelineStack extends cdk.Stack {
         cloudAssemblyArtifact,
         // We override the default install command to prepare our lambda too
         installCommand: 'npm ci --prefix cdk && npm ci --prefix lambda',
-        synthCommand: 'cd cdk; npx cdk synth; cd ..;',
+        // synthCommand: 'cd cdk; npx cdk synth; cd ..;',
         // As we may need Docker we create a privileged container
+        subdirectory: 'cdk',
         environment: {
           privileged: true,
         },
